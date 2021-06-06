b02f95f3dd4f325e1aa2460e0b2d1a1b52b93b05
lindamai <84297467+lindamai@users.noreply.github.com>
2021-06-05 20:38:30 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -47,16 +47,14 @@ WARNING!!! WARNING!!!
   // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
   // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
   // name in the loop.
-(function () {
-var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
- for (var i = 0; i < names.length; i++) {
-   var firstLetter = names[i].charAt(0).toLowerCase();
-      if (firstLetter == 'j') {
-  if (firstLetter === 'j') {
-    byeSpeaker.speak(names[i]);
-  } else {
-    helloSpeaker.speak(names[i]);
-  }
-}
-
-})();
+ (function () {
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+    for (var i = 0; i < names.length; i++) {
+      var firstLetter = names[i].charAt(0).toLowerCase();
+      if (firstLetter === 'j') {
+        byeSpeaker.speak(names[i]);
+      } else {
+        helloSpeaker.speak(names[i]);
+      }
+    }
+  })();
