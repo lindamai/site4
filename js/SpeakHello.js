33217b0f21f0f4415d301133baae7a7248ef7a4c
lindamai <84297467+lindamai@users.noreply.github.com>
2021-06-05 20:05:48 -0400

    Update SpeakHello.js

--- js/SpeakHello.js
+++ js/SpeakHello.js
@@ -1,7 +1,7 @@
 (function(window) {	
 	var speakWord = "Hello";
 	var helloSpeaker = {};	
-	helloSpeaker. speak = function (name) {
+	helloSpeaker.speak = function (name) {
 		console.log(speakWord + " " + name);
 	}
 	window.helloSpeaker =  helloSpeaker;
